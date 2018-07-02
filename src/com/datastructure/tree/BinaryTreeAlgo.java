911751a840e2608efde5ea9e439dbab8ccf9fdfb
rajnish1892 <rajnish1892@gmail.com>
2018-07-03 03:15:15 +0530

    Code changes for tree traversal algos

--- src/com/datastructure/tree/BinaryTreeAlgo.java
+++ src/com/datastructure/tree/BinaryTreeAlgo.java
@@ -24,8 +24,8 @@ public class BinaryTreeAlgo {
 		treeValueList.add(105);
 		treeValueList.add(69);
 		BinaryTreeAlgo binaryTreeAlgo = new BinaryTreeAlgo();
-		BinaryTreeNode binaryTree = binaryTreeAlgo
-				.createBinaryTree(treeValueList);
+		BinaryTreeNode binaryTree = binaryTreeAlgo.createBinaryTree(treeValueList);
+		binaryTreeAlgo.postOrder(binaryTree);
 
 	}
 
@@ -45,17 +45,13 @@ public class BinaryTreeAlgo {
 	}
 
 	public void add(int value, BinaryTreeNode rootNode) {
-		if (rootNode.getValue() > value
-				&& rootNode.getBinaryTreeLeftChild() != null) {
+		if (rootNode.getValue() > value && rootNode.getBinaryTreeLeftChild() != null) {
 			add(value, rootNode.getBinaryTreeLeftChild());
-		} else if (rootNode.getValue() < value
-				&& rootNode.getBinaryTreeRightChild() != null) {
+		} else if (rootNode.getValue() < value && rootNode.getBinaryTreeRightChild() != null) {
 			add(value, rootNode.getBinaryTreeRightChild());
-		} else if (rootNode.getValue() > value
-				&& rootNode.getBinaryTreeLeftChild() == null) {
+		} else if (rootNode.getValue() > value && rootNode.getBinaryTreeLeftChild() == null) {
 			rootNode.setBinaryTreeLeftChild(new BinaryTreeNode(value));
-		} else if (rootNode.getValue() > value
-				&& rootNode.getBinaryTreeRightChild() == null) {
+		} else if (rootNode.getValue() < value && rootNode.getBinaryTreeRightChild() == null) {
 			rootNode.setBinaryTreeRightChild(new BinaryTreeNode(value));
 		}
 	}
@@ -65,14 +61,43 @@ public class BinaryTreeAlgo {
 	}
 
 	public void inOrder(BinaryTreeNode rootNode) {
-
+		if(rootNode.getBinaryTreeLeftChild()==null){
+			System.out.println(rootNode.getValue());
+		}else{
+			inOrder(rootNode.getBinaryTreeLeftChild());
+			System.out.println(rootNode.getValue());
+			if(rootNode.getBinaryTreeRightChild()!=null){
+				inOrder(rootNode.getBinaryTreeRightChild());
+			}
+		}
 	}
 
 	public void preOrder(BinaryTreeNode rootNode) {
+		System.out.println(rootNode.getValue());
+		if(rootNode.getBinaryTreeLeftChild()!=null){
+			preOrder(rootNode.getBinaryTreeLeftChild());
+			if(rootNode.getBinaryTreeRightChild()!=null){
+				preOrder(rootNode.getBinaryTreeRightChild());
+			}
+		}
 
 	}
 
 	public void postOrder(BinaryTreeNode rootNode) {
+		if(rootNode.getBinaryTreeLeftChild()!=null){
+			postOrder(rootNode.getBinaryTreeLeftChild());
+			if(rootNode.getBinaryTreeRightChild()!=null){
+				postOrder(rootNode.getBinaryTreeRightChild());
+			}
+		}
+		System.out.println(rootNode.getValue());
 
+	}
+	public void bfsTraversal(BinaryTreeNode rootNode){
+		System.out.println(rootNode.getValue());
+		System.out.println(rootNode.getBinaryTreeLeftChild().getValue());
+		System.out.println(rootNode.getBinaryTreeRightChild().getValue());
+		bfsTraversal(rootNode.getBinaryTreeLeftChild());
+		bfsTraversal(rootNode.getBinaryTreeRightChild());
 	}
 }
